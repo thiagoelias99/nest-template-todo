f97e4c5de5b54d2433b064d59d01bef1187700b1
Thiago Elias <thiagoelias99@gmail.com>
2024-01-20 09:52:56 -0300

    fix: :bug: Status Code
    Fixed status code from default 201 to 200

--- src/resources/auth/auth.controller.ts
+++ src/resources/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post } from '@nestjs/common'
 import { AuthService, AuthResponse } from './auth.service'
 import { AuthDto } from './auth.dto'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
@@ -9,6 +9,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Post('login')
+  @HttpCode(200)
   @ApiOperation({ summary: 'Login and receive JWT Token' })
   @ApiResponse({
     status: 200,
